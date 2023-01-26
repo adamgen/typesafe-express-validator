0d032c43b0ffa2b7027d87b39d810b18a2dc9070
adamgen <adamgensh@gmail.com>
2023-01-26 12:18:57 +0200

    Write and read typesafe

--- tests/index.ts
+++ tests/index.ts
@@ -1,6 +1,18 @@
 import { ExpressValidatorToSchema } from '../lib/ExpressValidatorToSchema';
-import { Schema } from 'express-validator';
-const schema = {
+import { ParamSchema, Schema } from 'express-validator';
+
+type MyKeys =
+  | 'name'
+  | 'resource_tag_ids'
+  | 'resource_tag_ids.*'
+  | 'rsa_private'
+  | 'rsa_public'
+  | 'id';
+
+const defineSchema = <T extends Schema>(schema: T & Schema): T =>
+  schema as unknown as T;
+
+const schema = defineSchema({
   name: {
     in: 'body',
     isString: true,
@@ -36,12 +48,16 @@ const schema = {
     isInt: true,
     exists: { negated: true },
   },
-} as const;
+} as const);
 
 type TypeofSchema = typeof schema;
+type Keys = keyof TypeofSchema;
+type Value = {
+  [key in Keys]: TypeofSchema[key];
+};
 
 // export type ExpressValidatorToSchema<T extends Schema> = {
 //   [key in keyof RawSchema<T>]: RawSchema<T>[key];
 // };
 
-type mySchema = ExpressValidatorToSchema<TypeofSchema >;
+type mySchema = ExpressValidatorToSchema<Value>;
